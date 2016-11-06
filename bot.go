148422c46e2f39bdc0689aff01f4b4020ff36181
David Grzebinski <dwg7486@rit.edu>
2016-11-06 09:05:10 -0500

    Added basic commands

--- bot.go
+++ bot.go
@@ -5,26 +5,76 @@ import (
     "os/signal"
     "fmt"
     "flag"
+    "strings"
+    "bufio"
 
     "github.com/bwmarrin/discordgo"
 )
 
-func handleOnReady(session *discordgo.Session, event *discordgo.Ready) {
-    session.UpdateStatus(0, "")
+var (
+    session *discordgo.Session
+
+    OWNER_ID string
+
+    /* temporary constants */
+    GENERAL_CHANNEL string = "162620290487025674"
+
+)
+
+func handleOnReady(s *discordgo.Session, ev *discordgo.Ready) {
+    s.UpdateStatus(0, "")
 }
+
+func handleMessageCreate(s *discordgo.Session, ev *discordgo.MessageCreate) {
+
+}
+
+func acceptStdIn() {
+    reader := bufio.NewReader(os.Stdin)
+    for {
+        fmt.Print(">")
+        text, _ := reader.ReadString('\n')
+        parseCommand(text)
+    }
+}
+
+func parseCommand(input string) {
+    splitIn := strings.SplitN(input, " ", 2)
+    cmdKey := splitIn[0]
+    cmdBody := splitIn[1]
+
+    switch cmdKey {
+
+    case "say":
+        session.ChannelMessageSend(GENERAL_CHANNEL, cmdBody)
+
+    case "tts":
+        session.ChannelMessageSendTTS(GENERAL_CHANNEL, cmdBody)
+
+    }
+}
+
 
 func main() {
     var (
         Token = flag.String("t", "", "Discord Auth Token")
         Owner = flag.String("o", "", "Bot Owner ID")
+        err error
     )
     flag.Parse()
 
-    fmt.Println(*Owner)
+    if *Token == "" {
+        fmt.Println("Auth Token is required")
+    }
+
+    if *Owner == "" {
+        fmt.Println("Owner ID is required")
+        return
+    }
 
     fmt.Println("Creating Discord session")
 
-    session, err := discordgo.New(*Token)
+    session, err = discordgo.New(*Token)
 
     if err != nil {
         fmt.Println("Error creating Discord session")
@@ -32,8 +82,11 @@ func main() {
     }
 
     session.AddHandler(handleOnReady)
+    session.AddHandler(handleMessageCreate)
 
     session.Open()
+
+    go acceptStdIn()
 
     fmt.Println("Session initialization finished")
 
